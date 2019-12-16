0a07355eaafe69267249b244a73cd67ec61ebfba
pearisgreen <gerrit.proessl@gmail.com>
2019-12-16 23:37:00 +0100

    update: nc able to send hex data

--- src/nc/nc.pl
+++ src/nc/nc.pl
@@ -37,16 +37,14 @@ if (not $pid) {
   exit;
 }
 
-sub parse_input {
-  my $in = $_[0];
-  if ($in =~ m/:s/) {
-    return substr($in, 2, length $in);
-  }
-}
-
 while ($running) {
   my $in = <STDIN>;
-  $socket->send($in);
+  if ($in =~ m/:s /) {
+    $socket->send(substr($in, 3));
+  }
+  if ($in =~ m/:x /) {
+    $socket->send(pack("H*", substr($in, 3, -1)));
+  }
 }
 
 wait();
